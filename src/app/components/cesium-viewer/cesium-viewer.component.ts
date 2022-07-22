068ea52977ace9c0789cf11bd355ec8c8ec34b92
Alexander Piskarev <lucky-pa777@yandex.ru>
2022-07-22 16:40:51 +0300

    master
    entities module

--- src/app/components/cesium-viewer/cesium-viewer.component.ts
+++ src/app/components/cesium-viewer/cesium-viewer.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import * as Cesium from 'cesium';
+import {BillboardCollection} from "cesium";
 
 @Component({
   selector: 'app-cesium-viewer',
@@ -65,7 +66,34 @@ export class CesiumViewerComponent implements OnInit {
       },
     });
 
+    let billboards = viewer.scene.primitives.add( new BillboardCollection());
 
+    billboards.add({
+
+      position : Cesium.Cartesian3.fromDegrees(37.175657,55.989027, 250.0),
+      image : './assets/test.png',
+      height: 30,
+      width: 30,
+    });
+    billboards.add({
+      position : Cesium.Cartesian3.fromDegrees(37.175657,56.989027, 0.0),
+      image : './assets/test.png',
+      height: 30,
+      width: 30,
+    });
+
+
+    // Материал с изображения
+    viewer.entities.add({
+      id: "Textured rectangle, zIndex 3",
+      rectangle: {
+        coordinates: Cesium.Rectangle.fromDegrees(-99.5, 20.0, -90.0, 30.0),
+        material: new Cesium.ImageMaterialProperty({
+          image: "./assets/cesium.jpg"
+        }),
+        zIndex: 3,
+      },
+    });
     console.log(viewer.scene)
     console.log(  viewer.scene.moon.isDestroyed())
   }
