2dafcee8641bffc9d471a4d0196a57c8c0874244
Alexander Piskarev <lucky-pa777@yandex.ru>
2022-07-22 15:56:25 +0300

    master
    глобальное освещение
    waterMask и vertexNormals

--- src/app/components/cesium-viewer/cesium-viewer.component.ts
+++ src/app/components/cesium-viewer/cesium-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as Cesium from 'cesium';
 
 @Component({
@@ -8,7 +8,8 @@ import * as Cesium from 'cesium';
 })
 export class CesiumViewerComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
 
@@ -18,20 +19,26 @@ export class CesiumViewerComponent implements OnInit {
     Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1NmQ1OGZlZC00Y2ZkLTRiMmEtYWYwNC0xYTRiYjZjODMxMzciLCJpZCI6MTAyMTU4LCJpYXQiOjE2NTg0ODUzOTZ9.iFLj0GX0uOMvS0hPyjkhfyK9JygdishYZIdag5xm3PY';
     // @ts-ignore
     window.CESIUM_BASE_URL = '/assets/cesium/';
-// Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+
     const viewer = new Cesium.Viewer('cesiumContainer', {
-      terrainProvider: Cesium.createWorldTerrain()
+      terrainProvider: Cesium.createWorldTerrain({
+        requestWaterMask: true,
+        requestVertexNormals : true
+      }),
     });
-// Add Cesium OSM Buildings, a global 3D buildings layer.
     const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
+
+    viewer.extend(Cesium.viewerCesiumInspectorMixin);
+    viewer.scene.moon = new Cesium.Moon(); // Почему не работает?
+
+    viewer.scene.sun = new Cesium.Sun() // И это тоже
     viewer.camera.flyTo({
-      destination : Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-      orientation : {
-        heading : Cesium.Math.toRadians(0.0),
-        pitch : Cesium.Math.toRadians(-15.0),
-      }
+      destination: Cesium.Cartesian3.fromDegrees( 37.175657,55.989027, 800),
     });
+
+    viewer.scene.globe.enableLighting = true;
+    console.log(viewer.scene)
+    console.log(  viewer.scene.moon.isDestroyed())
   }
 
 }
